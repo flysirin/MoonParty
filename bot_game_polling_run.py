72fcf77e48dd4e3a1eeb1c3490746daafa30c96b
Stanislaus <flysirin@gmail.com>
2023-07-02 01:43:12 +0400

    move Storage and Redis to models.methods

--- bot_game_polling_run.py
+++ bot_game_polling_run.py
@@ -3,9 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
-from aiogram.filters.state import State, StatesGroup
-from aiogram.fsm.storage.redis import Redis, RedisStorage
-
+from models.methods import storage
 
 from handlers import user_handlers, admin_handlers
 from config_data.config import BOT_TOKEN, ADMIN_IDS
@@ -14,12 +12,6 @@ logging.basicConfig(level=logging.WARNING)
 logger_main_file = logging.getLogger(__name__)
 
 bot = Bot(token=BOT_TOKEN)
-
-# Initialize  Redis storage
-redis = Redis(host='localhost')
-storage = RedisStorage(redis=redis)
-
-
 dp = Dispatcher(storage=storage)
 
 # Create database of users
@@ -28,8 +20,8 @@ user_dict: dict[int, dict[str, str | int | bool]] = {}
 # bot.delete_webhook(drop_pending_updates=True)
 
 # Register routers in Dispatcher
-dp.include_router(user_handlers.router)
-dp.include_router(admin_handlers.router)
+dp.include_router(admin_handlers.router_admin)
+dp.include_router(user_handlers.router_user)
 
 
 if __name__ == '__main__':
