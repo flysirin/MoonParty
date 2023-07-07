7b8b59a05ed75ad2e65970a2f5c1abce5c1a7ebf
Stanislaus <flysirin@gmail.com>
2023-07-08 00:17:42 +0400

    add lexicon_commands

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,3 +1,14 @@
 LEXICON: dict[str, str] = {
-    '/start': 'Hello! \n Get start the game!\n',
-    '/help': 'Game info', }
+    '/start': 'Hello! \n Reaction on command start from user without registration\n',
+    '/help': 'Game info',
+    '/start_admin': 'Hello for Admin!',
+    '/help_admin': 'Help info for admin',
+
+}
+
+LEXICON_COMMANDS: dict[str: str] = {
+    '/start': 'Init connect with bot',
+    '/help': 'Information about bot',
+    # '/command_3': 'command_3 desription',
+    # '/command_4': 'command_4 desription'
+}
