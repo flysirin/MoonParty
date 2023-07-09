17c5993bac925f389ce61732733e6933b3e52015
Stanislaus <flysirin@gmail.com>
2023-07-10 00:54:08 +0400

    add state wait_register_players, game_process

--- states/room_leader_states.py
+++ states/room_leader_states.py
@@ -2,13 +2,14 @@ from aiogram.filters.state import StatesGroup, State
 
 
 # Create class heritable from StateGroup for states our FSM
-class FSMAdminRoom(StatesGroup):
+class FSMRoomLeader(StatesGroup):
     """Create instances of the State class, sequentially
     listing the possible states it will be in
     bot at different moments of interaction with the admin"""
 
     input_room_name = State()
     input_password = State()
+    wait_register_players = State()
+    game_process = State()
     game_setting_process = State()
 
-    game_process = State()
