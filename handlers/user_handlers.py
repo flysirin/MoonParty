6a7494c6f823503cdb8f06ce10295c1f1237c097
Stanislaus <flysirin@gmail.com>
2023-07-10 00:50:10 +0400

    upd user handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Bot, Router, F
 
 from aiogram.fsm.context import FSMContext
@@ -6,9 +8,10 @@ from states.user_states import FSMUser
 
 from aiogram.filters import Command, CommandStart, StateFilter, Text
 from filters.member_filters import IsRegisterUsers
-
 from aiogram.types import Message, CallbackQuery
 from lexicon.lexicon import LEXICON
+from keyboards import user_keyboards
+from services.other import get_active_rooms
 
 import logging
 
@@ -22,12 +25,18 @@ router_user.message.filter(IsRegisterUsers())
 @router_user.message(CommandStart(), StateFilter(default_state))
 async def process_start_command(message: Message):
     logger_user_handler.info(f"Start for registered user, id: {message.from_user.id}")
-    await message.answer(text=LEXICON["/start"])
+    await message.answer(text=LEXICON["/start"],
+                         reply_markup=user_keyboards.start_kb())
 
 
 @router_user.message(Command(commands=["help"]), StateFilter(default_state))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON["/help"])
 
-# @router.message(CommandStart(), StateFilter(FSMUser.in_game))
-# async def process_
+
+@router_user.message(Text("Show active rooms"))
+async def show_active_rooms(message: Message, state: FSMContext):
+    active_rooms = await get_active_rooms()
+    await message.answer(text=f"Active rooms. Press for enter.",
+                         reply_markup=user_keyboards.active_rooms(**active_rooms))
+    await asyncio.sleep(1)
