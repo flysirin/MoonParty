99160ae6b33215fe498b8df1a0ded07e82f56d6d
Stanislaus <flysirin@gmail.com>
2023-07-12 23:42:54 +0400

    add registration in room

--- services/user_services.py
+++ services/user_services.py
@@ -17,6 +17,18 @@ async def get_active_rooms():
     for nickname, user_id in active_leaders.items():
         data_form_leader = await get_data_from_user(bot=bot_object, user_id=user_id)
         room_name = data_form_leader.get('room_name', None)
-        if data_form_leader.get('is_active', False):
+        if data_form_leader.get('active_registration', False):
             active_rooms.update({room_name: user_id})
     return active_rooms
+
+
+async def register_user_in_room(username: str = None, user_id: int = None, leader_id: int = None):
+    players = (await get_data_from_user(bot=bot_object, user_id=leader_id)).get('players', {})
+    if not players:
+        await update_data_from_user(bot=bot_object, user_id=leader_id, data={'players': {}})
+    player = {user_id: {'nickname': username, 'role': None, 'lives': 10, 'is_speaker': False,
+                        'is_werewolf': False, 'did_he_say': False, 'is_change_role': False,
+                        'who_checked_role': []}}
+    players.update(player)
+    await update_data_from_user(bot=bot_object, user_id=leader_id, data={'players': players})
+
