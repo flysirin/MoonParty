a2647facab84107fe57fe864a8a36c9c4615cf9b
Stanislaus <flysirin@gmail.com>
2023-07-12 23:44:53 +0400

    upd lexicon

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -12,3 +12,37 @@ LEXICON_COMMANDS: dict[str: str] = {
     # '/command_3': 'command_3 desription',
     # '/command_4': 'command_4 desription'
 }
+
+LEADER_LEXICON: dict[str:str] = {
+    "/start": "Hello! \n Reaction on command start from user without registration\n",
+    "/help": "Information about bot",
+    "start for registered": "Start for registered user, id: ",
+    "Game data cleared": "Game data cleared",
+    "Message state of play": "You are in a state of play. Finish the game to go to the main menu.",
+    "Waiting for a room name:": 'Waiting for a room name:',
+    "Can't start without Room Name or pass": "Can't start without Room Name or pass",
+    "Message waiting players": f"Waiting for players registration. "
+                               f"Registering is available before start game",
+    "Cancel": "Cancel",
+    "Click to exit": "Click to exit",
+
+
+
+}
+
+
+USER_LEXICON: dict[str:str] = {
+    "/start": "Hello! \n Reaction on command start from user without registration\n",
+    "/help": "Information about bot",
+    "active rooms": "Active rooms. Press for enter.",
+    "no active rooms": "There are currently no active rooms",
+    "Show active rooms": "Show active rooms",
+    "Input password for room: ": "Input password for room: ",
+    "Input password: ": "Input password: ",
+    "Password entry error": "Password entry error. Try again or press cancel",
+    "Click to exit": "Click to exit",
+    "Cancel": "Cancel",
+    "Successful registered in room: ": "Successful registered in room: ",
+
+}
+
