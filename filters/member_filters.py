57984fbc9c0ac7a92da2a02e892cb7bb0e6e3acd
Stanislaus <flysirin@gmail.com>
2023-07-17 21:58:06 +0400

    add CheckUserConnectFilter, CheckSpeakerFilter

--- filters/member_filters.py
+++ filters/member_filters.py
@@ -4,6 +4,7 @@ from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from config_data.config import ADMIN_IDS
 from bot_object import bot_object
+from services.user_services import get_user_id_by_serial_number
 
 _MAIN_ADMINS: list[int] = [int(admin.strip()) for admin in ADMIN_IDS.split(',')]
 
@@ -37,4 +38,34 @@ class CheckPassFilter(BaseFilter):
         leader_data = await get_data_from_user(bot=bot_object, user_id=int(select_leader_id))
         leader_pass = leader_data.get("password", "")
         return leader_pass == message.text
+
+
+class CheckSpeakerFilter(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        user_id = message.from_user.id
+        leader_id = (await get_data_from_user(bot_object, user_id)).get('select_leader_id', 0)
+        speaker_id = (await get_data_from_user(bot_object, leader_id)).get('current_speaker', '')
+
+        return speaker_id != user_id
+
+
+class CheckUserConnectFilter(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        user_id = message.from_user.id
+        serial_user_num = message.text.split()[-1]
+        if not serial_user_num.isdigit():
+            return False
+
+        leader_id = (await get_data_from_user(bot_object, user_id=user_id)).get('select_leader_id', 0)
+        if leader_id:
+            players = (await get_data_from_user(bot_object, user_id=leader_id)).get('players', {})
+            is_active_game = players.get('active_game', False)
+            is_alive_not_winner = 0 < players[user_id]['lives'] < 20
+            not_speaker = players.get('current_speaker', '') != user_id
+            correct_number = int(serial_user_num) in {number.get('user_number', '') for number in players.values()}
+
+            return is_alive_not_winner and not_speaker and is_active_game and correct_number
+
+
+
 
