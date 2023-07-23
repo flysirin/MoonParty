cc41798d6197b2c1df176c9439daf133de51889b
Stanislaus <flysirin@gmail.com>
2023-07-24 00:37:13 +0400

    add host default settings

--- config_data/config.py
+++ config_data/config.py
@@ -6,4 +6,13 @@ env.read_env()
 BOT_TOKEN = env("BOT_TOKEN")
 ADMIN_IDS = env("ADMIN_IDS")
 
-
+host_game_data: dict = {
+    'settings':
+        {
+            'toast_time': 60,
+            'percent_role': {'human': 40, 'wolf': 40, 'werewolf': 20},
+            'start_lives': 10,
+            'win_lives': 20,
+            'count_winners': 1,
+        }
+}
