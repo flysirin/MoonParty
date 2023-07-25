12a6f2dfc54cd1809e93e85b086eba1112df2146
Stanislaus <flysirin@gmail.com>
2023-07-26 00:07:49 +0400

    add menu keyboards

--- keyboards/host_keyboards.py
+++ keyboards/host_keyboards.py
@@ -21,15 +21,15 @@ def registered_players_inline_kb() -> InlineKeyboardMarkup:
                                         callback_data=f"_view_qr_code_pressed_")
     select_players = InlineKeyboardButton(text=HOST_BUTTONS["View players"],
                                           callback_data=f"_select_players_pressed_")
-    game_setting = InlineKeyboardButton(text=HOST_BUTTONS["Game setting"],
-                                        callback_data="_setting_game_pressed_")
+    # game_setting = InlineKeyboardButton(text=HOST_BUTTONS["Game setting"],
+    #                                     callback_data="_setting_game_pressed_")
     exit_main_menu = InlineKeyboardButton(text=HOST_BUTTONS["Exit"],
                                           callback_data=f"_exit_main_menu_pressed_")
     keyboard = InlineKeyboardMarkup(inline_keyboard=[[update_user_list],
                                                      [start_game],
                                                      [view_qr_code],
                                                      [select_players],
-                                                     [game_setting],
+                                                     # [game_setting],
                                                      [exit_main_menu],
                                                      ], )
     return keyboard
@@ -54,7 +54,7 @@ def select_players_inline_kb(**kwargs) -> InlineKeyboardMarkup:
     return kb_builder.as_markup()  # return object inline kb
 
 
-def select_one_player_inline_kb(player: str) -> InlineKeyboardMarkup:
+def select_one_player_inline_kb(player: int) -> InlineKeyboardMarkup:
     delete_player = InlineKeyboardButton(text=HOST_BUTTONS["Delete"],
                                          callback_data=f"_delete_player_id_{player}_pressed_")
     exit_to_select_players = InlineKeyboardButton(text=HOST_BUTTONS["Exit"],
@@ -70,21 +70,6 @@ def hide_qr_inline_kb() -> InlineKeyboardMarkup:
                                        callback_data=f"__hide__qr_code__")
     keyboard = InlineKeyboardMarkup(inline_keyboard=[[hide_button]])
     return keyboard
-
-
-def game_process_menu_inline_kb(**kwargs) -> InlineKeyboardMarkup:
-    kb_builder = InlineKeyboardBuilder()
-    buttons: list[InlineKeyboardButton] = [
-        InlineKeyboardButton(text=HOST_BUTTONS["Finish game"], callback_data=f"_finish_game_pressed_")]
-
-    if kwargs:
-        for user_id, annotation in kwargs.items():
-            nickname = annotation.get('nickname', 'nickname')
-            buttons.append(InlineKeyboardButton(text=f"{HOST_BUTTONS['Player: ']}{nickname}",
-                                                callback_data=f"__{user_id}__player__user_id__"))
-
-    kb_builder.row(*buttons, width=1)
-    return kb_builder.as_markup()  # return object inline kb
 
 
 def game_settings_inline_kb() -> InlineKeyboardMarkup:
@@ -189,4 +174,60 @@ def spread_roles_inline_kb():
     ]
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
+def confirm_finish_game_process_inline_kb() -> InlineKeyboardMarkup:
+    confirm_finish_button = InlineKeyboardButton(text=HOST_BUTTONS["Confirm finish game process"],
+                                                 callback_data="_confirm_finish_game_pressed_")
+    return_button = InlineKeyboardButton(text=HOST_BUTTONS["Exit"],
+                                         callback_data="_return_game_process_pressed_")
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[confirm_finish_button],
+                                                     [return_button],
+                                                     ])
+    return keyboard
+
+
+def game_process_menu_inline_kb() -> InlineKeyboardMarkup:
+    update_button = InlineKeyboardButton(text=HOST_BUTTONS["Update info"],
+                                         callback_data=f"_update_host_data_pressed_")
+    select_players_button = InlineKeyboardButton(text=HOST_BUTTONS["Up / Down - lives"],
+                                                 callback_data=f"_select_players_in_game_pressed_")
+    finish_game_button = InlineKeyboardButton(text=HOST_BUTTONS["Finish game"],
+                                              callback_data=f"_finish_game_pressed_")
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[update_button],
+                                                     [select_players_button],
+                                                     [finish_game_button],
+                                                     ])
+    return keyboard
+
+
+def select_players_in_game_inline_kb(**kwargs) -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+    return_button = InlineKeyboardButton(text=HOST_BUTTONS["Exit"],
+                                         callback_data=f"_return_game_process_pressed_")
+    if kwargs:
+        for user_id, annotation in kwargs.items():
+            nickname = annotation.get('nickname', 'nickname')
+            buttons.append(InlineKeyboardButton(text=f"🧑 {nickname}",
+                                                callback_data=f"__{user_id}__player__user_id__"))
+
+    buttons.append(return_button)
+    kb_builder.row(*buttons, width=1)
+    return kb_builder.as_markup()  # return object inline kb
+
+
+def select_one_player_in_game_inline_kb(player_id: int) -> InlineKeyboardMarkup:
+    up_lives_player = InlineKeyboardButton(text=HOST_BUTTONS["Up lives"],
+                                           callback_data=f"_up___lives_player_id_{player_id}_pressed_")
+    down_lives_player = InlineKeyboardButton(text=HOST_BUTTONS["Down lives"],
+                                             callback_data=f"_down_lives_player_id_{player_id}_pressed_")
+
+    exit_to_select_players = InlineKeyboardButton(text=HOST_BUTTONS["Exit"],
+                                                  callback_data=f"_exit_select_players_pressed_")
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[down_lives_player, up_lives_player],
+                                                     [exit_to_select_players],
+                                                     ])
     return keyboard
